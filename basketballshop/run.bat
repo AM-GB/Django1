6780dc9f51fdcb649a7843bb4f38ae2f19acf69f
AM <anatolminor91@inbox.ru>
2021-01-24 21:39:47 +0300

    HW1

---
+++ basketballshop/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+start cmd.exe /C "python manage.py runserver && cd G:\GeekBrains\9_Django_Framework_base\DjangoHW\Django1\basketballshop && D:"
+start C:\"Program Files (x86)"\Google\Chrome\Application\chrome.exe "http://127.0.0.1:8000/"
