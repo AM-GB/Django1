aa5f6932db06b49fe7c858d9214f969f71045bfa
anatol <anatolminor91@inbox.ru>
2021-04-18 10:45:30 +0300

    HW5

--- basketballshop/basketballshop/settings.py
+++ basketballshop/basketballshop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*7pox(s=_$#=r@ig1wg-d_9e+-%==*24woza7!fbn0vnngf%81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
